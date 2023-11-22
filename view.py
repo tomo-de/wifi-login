7e41213237f53b696c4ec0ddce6b82e50e3cfcfa
tomo-de <71959137+tomo-de@users.noreply.github.com>
2023-11-22 18:30:37 +0900

    [upsate] 設定画面を作成

--- view.py
+++ view.py
@@ -1,16 +1,75 @@
 import rumps
 import flet as ft
+import data
 
 
-def main(page: ft.Page):
+def config_page(page: ft.Page):
     # add/update controls on Page
-    pass
+    page.title = "設定"
+    page.window_width = 600
+    page.window_height = 400
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+    d = data.preference()
+    uid = ft.TextField(
+        value=d.user_id, text_align=ft.TextAlign.RIGHT, width=300)
+    pwd = ft.TextField(
+        value=d.password, text_align=ft.TextAlign.RIGHT, width=300, password=True, can_reveal_password=True)
+    url = ft.TextField(
+        value=d.url, text_align=ft.TextAlign.RIGHT, width=300, hint_text="https://example.com/")
+    ssid_list = ft.TextField(
+        value=",".join(d.ssid_list), text_align=ft.TextAlign.RIGHT, width=300, hint_text="xxx,yyy,zzz")
+    
+    def handle_click_apply_button(e):
+        d = data.preference()
+        d.set_user_id(uid.value)
+        d.set_password(pwd.value)
+        d.set_url(url.value)
+        d.set_ssid_list(ssid_list.value)
+        page.update()
+
+    page.add(
+        ft.Column(
+            [
+                # uid
+                ft.Row(
+                    [
+                        ft.Text(value="ログインID"),
+                        uid
+                    ]
+                ),
+                # pwd
+                ft.Row(
+                    [
+                        ft.Text(value="パスワード"),
+                        pwd
+                    ]
+                ),
+                ft.Row(
+                    [
+                        ft.Text(value="url"),
+                        url
+                    ]
+                ),
+                ft.Row(
+                    [
+                        ft.Text(value="ssidのリスト"),
+                        ssid_list
+                    ]
+                ),
+                ft.Container(
+                    alignment=ft.alignment.center_right,
+                    content=ft.FilledButton(text="適用", on_click=handle_click_apply_button)
+                )
+                
+            ]
+        )
+    )
 
 class App(rumps.App):
-    @rumps.clicked("Config")
-    def hello_world(self, _):
-        ft.app(target=main)
+    @rumps.clicked("設定")
+    def config(self, _):
+        ft.app(target=config_page)
 
     def login(self, sender):
         pass
